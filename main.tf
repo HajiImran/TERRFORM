6abe7d61a967014c8f8f4ef70dbbdaa7363553de
HajiImran <h.imran567@gmail.com>
2024-12-05 14:09:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-# Specify the provider and region
 provider "aws" {
-  region = "ap-south-1"
+region = "ap-south-1"
 }
 
-# Create an EC2 instance
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0327f51db613d7bd2 (64-bit (x86)) / ami-08d59e179a2b20f3d (64-bit (Arm))" # Example AMI, replace with a valid AMI ID
+resource "aws_instance" "one" {
+  count         = 5
+  ami           = "ami-0327f51db613d7bd2 (64-bit (x86)) / ami-08d59e179a2b20f3d (64-bit (Arm))"
   instance_type = "t2.micro"
-  key_name      = "TIER1" # Replace with your SSH key name
-
-
-
+  key_name      = "TIER1"
   tags = {
-    Name = "MyEC2Instance"
+    Name = "test-server"
   }
 }
