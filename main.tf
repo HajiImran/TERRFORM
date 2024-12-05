ff919ac809765f39f253463d3a3e6c0ba0cc7765
HajiImran <h.imran567@gmail.com>
2024-12-05 13:14:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.73.0"
-    }
-  }
-}
 
 provider "aws" {
 region = "ap-south-1"
@@ -16,12 +8,7 @@ count = 5
 ami = "ami-0327f51db613d7bd2"
 instance_type = "t2.medium"
 key_name = "TIER1"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
 tags = {
 Name = var.instance_names[count.index]
 }
-}
-
-variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
