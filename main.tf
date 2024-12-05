d31f271bd05f95ac2a2ee1abd9d1f190b400ad6a
HajiImran <h.imran567@gmail.com>
2024-12-05 14:14:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
   count         = 5
-  ami           = "ami-0327f51db613d7bd2 (64-bit (x86))"
+  ami           = "ami-0327f51db613d7bd2"
   instance_type = "t2.micro"
   key_name      = "TIER1"
   tags = {
