507738805f6b38bc0f710b299c40a9b6dace800e
HajiImran <h.imran567@gmail.com>
2024-12-05 13:40:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.2"
-    }
-  }
-}
 provider "aws" {
 region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0327f51db613d7bd2"
-instance_type = "t2.medium"
-key_name = "TIER1"
+  count         = 5
+  ami           = "ami-0327f51db613d7bd2"
+  instance_type = "t2.micro"
+  key_name      = "dockerr"
+  vpc_security_group_ids = ["sg-05cfa7da6d7e8def3"]
+  tags = {
+    Name = "test-server"
+  }
 }
