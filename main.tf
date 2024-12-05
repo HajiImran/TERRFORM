3f046a737bc3363591b779743a7af78a77f00eee
HajiImran <h.imran567@gmail.com>
2024-12-05 14:19:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count         = 5
   ami           = "ami-0327f51db613d7bd2"
   instance_type = "t2.micro"
-  key_name      = "TIER1"
+  key_name      = "T1ER1"
   tags = {
     Name = "test-server"
   }
